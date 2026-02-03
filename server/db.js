e63b0e77a2dcc1db7b26f051b918dd4f98f7326b
Miggy-dev <juanmiguelbarbosa11@gmail.com>
2026-02-03 16:26:06 +0800

    chore: add check for missing DATABASE_URL

--- server/db.js
+++ server/db.js
@@ -3,6 +3,10 @@ import {Pool} from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
+if (!process.env.DATABASE_URL) {
+    console.error('CRITICAL ERROR: DATABASE_URL is not defined in environment variables!');
+}
+
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
